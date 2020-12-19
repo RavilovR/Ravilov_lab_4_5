546cb2eb5acde4fa31144b347dea936042b6e5ce
yakol <ya.kolysh@gmail.com>
2020-12-19 21:42:07 +0300

    Initial commit

---
+++ src/mirea/l5/Main.java
@@ -0,0 +1,8 @@
+package mirea.l5;
+
+public class Main {
+    public static void main(String[] args) {
+        MovableCircle circle1 = new MovableCircle(1, 2, 5, 6, 10);
+        System.out.println(circle1.center.toString());
+    }
+}
